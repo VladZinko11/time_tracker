7cd001bc56a7c11d69738f54e0f5b27fc613edab
VladZinko <zinko1996@yandex.ru>
2024-07-11 00:52:15 +0300

    created entity Record and Task

---
+++ src/main/java/com/zinko/time_tracker/data/entity/Record.java
@@ -0,0 +1,36 @@
+package com.zinko.time_tracker.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "records")
+public class Record {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "finish_time")
+    private LocalDateTime finishTime;
+
+    @Column(name = "time")
+    private Long time;
+
+}
---
+++ src/main/java/com/zinko/time_tracker/data/entity/Task.java
@@ -0,0 +1,28 @@
+package com.zinko.time_tracker.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Record> records;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+}
