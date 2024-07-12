ff1be4d245674692ab9b483c66a690126bb7985c
VladZinko <zinko1996@yandex.ru>
2024-07-13 01:46:32 +0300

    changed imports from * to single imports

--- src/main/java/com/zinko/time_tracker/web/controller/RecordController.java
+++ src/main/java/com/zinko/time_tracker/web/controller/RecordController.java
@@ -3,7 +3,14 @@ package com.zinko.time_tracker.web.controller;
 import com.zinko.time_tracker.service.RecordService;
 import com.zinko.time_tracker.service.dto.RecordDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
