36f31f65250e0d313c4cb0fb9b8abe25aec30b6c
VladZinko <zinko1996@yandex.ru>
2024-07-14 12:14:05 +0300

    Created jwtResponse class

---
+++ src/main/java/com/zinko/time_tracker/service/JwtResponse.java
@@ -0,0 +1,16 @@
+package com.zinko.time_tracker.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+}
+
+
